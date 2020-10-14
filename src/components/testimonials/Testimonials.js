8395db898b57eb6ce012139761b5f7b0dd5f42de
Ariel_Rodriguez <32606708+Ariels713@users.noreply.github.com>
2020-10-14 11:22:44 -0400

    fixed testimonials bug

--- src/components/testimonials/Testimonials.js
+++ src/components/testimonials/Testimonials.js
@@ -141,7 +141,7 @@ function SectionTestimonials() {
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
                 eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
                 enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat.
+                nisi ut aliquip ex ea.
               </h5>
             </Col>
           </Row>
